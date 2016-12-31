9ef99b957b3ac7956886835e031d7ee92db0e7c0
tapseet <geeksanad@gmail.com>
2017-01-01 01:56:41 +0200

    Update ttg.js

--- ttg.js
+++ ttg.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 	atOptions = {
 		'key' : 'ac7636f2958132912bb9c7dfb74b68fe',
 		'format' : 'iframe',
@@ -7,4 +7,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bnhtml.com/invoke.js"></scr' + 'ipt>');
-</script>
+
