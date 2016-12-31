ede7c0718bec19aa42ddab4d41014bafe9384ed6
tapseet <geeksanad@gmail.com>
2017-01-01 01:54:55 +0200

    Create ttg.js

---
+++ ttg.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'ac7636f2958132912bb9c7dfb74b68fe',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bnhtml.com/invoke.js"></scr' + 'ipt>');
+</script>
