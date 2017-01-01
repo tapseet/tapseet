7bc85c9bb1f176ce9b2fe5b34351e92e1954a6f1
tapseet <geeksanad@gmail.com>
2017-01-01 02:02:18 +0200

    Create 22344.js

---
+++ 22344.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '968e6450ce66195f917c9cf18c4339ed',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bnhtml.com/invoke.js"></scr' + 'ipt>');
