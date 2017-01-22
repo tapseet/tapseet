3790ef02ad984707974a4a6e5a881884737df08e
tapseet <geeksanad@gmail.com>
2017-01-22 02:58:24 +0200

    Create redirect3rd.js

---
+++ redirect3rd.js
@@ -0,0 +1,37 @@
+<script type='text/javascript'>
+function openLink(){
+    var myrandom=Math.round(Math.random()*28)
+    var links=new Array()
+    links[0]=&quot;http://www.javascriptkit.com&quot;
+    links[1]=&quot;http://www.dynamicdrive.com&quot;
+    links[2]=&quot;http://www.cssdrive.com&quot;
+    links[3]=&quot;http://www.codingforums.com&quot;
+    links[4]=&quot;http://www.news.com&quot;
+    links[5]=&quot;http://www.gamespot.com&quot;
+    links[6]=&quot;http://www.msnbc.com&quot;
+    links[7]=&quot;http://www.cnn.com&quot;
+    links[8]=&quot;http://news.bbc.co.uk&quot;
+    links[9]=&quot;http://www.news.com.au&quot;
+    links[10]=&quot;https://www.facebook.com/dfggdfgfgfdsgg&quot;
+    links[11]=&quot;https://maktoob.yahoo.com/?p=us&quot;
+    links[12]=&quot;https://www.google.com.eg/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=0ahUKEwibnuPdw9TRAhXKvBoKHU28CoEQFggzMAQ&url=https%3A%2F%2Fluckypatcher.co%2F&usg=AFQjCNHYZjrldbgPIbz2PPEOUuuKz4vbeA&sig2=ZY-WP0laST-IQ_ycr157Og&bvm=bv.144224172,bs.1,d.bGs&quot;
+    links[13]=&quot;https://nodistribute.com/&quot;
+    links[14]=&quot;https://partner.net.jumia.com/register.html?mid=517&quot;
+    links[15]=&quot;http://www.msnbc.com&quot;
+    links[16]=&quot;http://www.msnbc.com&quot;
+    links[17]=&quot;http://www.msnbc.com&quot;
+    links[18]=&quot;http://www.msnbc.com&quot;
+    links[19]=&quot;http://www.msnbc.com&quot;
+    links[20]=&quot;http://www.msnbc.com&quot;
+    links[21]=&quot;http://www.msnbc.com&quot;
+    links[22]=&quot;http://www.msnbc.com&quot;
+    links[23]=&quot;http://www.msnbc.com&quot;
+    links[24]=&quot;https://www.facebook.com/wahabadeliraq&quot;
+    links[25]=&quot;http://www.msnbc.com&quot;
+    links[26]=&quot;http://www.msnbc.com&quot;
+    links[27]=&quot;http://www.msnbc.com&quot;
+    links[28]=&quot;http://www.msnbc.com&quot;
+
+    window.location=links[myrandom]
+}
+</script>
